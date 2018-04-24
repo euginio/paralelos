b86894691bdbd2934537dc933ad582ea9160424b
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2018-04-24 15:01:55 -0300

    mc

--- pract2/ej4/s-sort.c
+++ pract2/ej4/s-sort.c
@@ -9,49 +9,49 @@ int N, P, *vector, *copia;
 //Para calcular tiempo
 double dwalltime()
 {
-  double sec;
-  struct timeval tv;
+    double sec;
+    struct timeval tv;
 
-  gettimeofday(&tv, NULL);
-  sec = tv.tv_sec + tv.tv_usec / 1000000.0;
-  return sec;
+    gettimeofday(&tv, NULL);
+    sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+    return sec;
 }
 
 int main(int argc, char *argv[])
 {
-  int i, check = 1;
-  double timetick;
+    int i, check = 1;
+    double timetick;
 
-  //Controla los argumentos al programa
-  if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || ((P = atoi(argv[2])) <= 0))
-  {
-    printf("\nUsar: %s n\n  n: Dimension del vector N\n", argv[0]);
-    exit(1);
-  }
+    //Controla los argumentos al programa
+    if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || ((P = atoi(argv[2])) <= 0))
+    {
+        printf("\nUsar: %s n\n  n: Dimension del vector N\n", argv[0]);
+        exit(1);
+    }
 
-  //Aloca memoria para las matrices
-  vector = (int *)malloc(sizeof(int) * N);
-  copia = (int *)malloc(sizeof(int) * N);
-  //Inicializa el vector con todos sus valores en 1
-  for (i = 0; i < N; i++)
-  {
-    vector[i] = 1;
-    copia[i] = 0;
-  }
-  //Realiza la búsqueda
-  timetick = dwalltime();
-  BottomUpMergeSort(vector, copia, N);
-  printf("Tiempo en segundos %f\n", dwalltime() - timetick);
+    //Aloca memoria para las matrices
+    vector = (int *)malloc(sizeof(int) * N);
+    copia = (int *)malloc(sizeof(int) * N);
+    //Inicializa el vector con todos sus valores en 1
+    for (i = 0; i < N; i++)
+    {
+        vector[i] = 1;
+        copia[i] = 0;
+    }
+    //Realiza la búsqueda
+    timetick = dwalltime();
+    BottomUpMergeSort(vector, copia, N);
+    printf("Tiempo en segundos %f\n", dwalltime() - timetick);
 
-  printf("prom %d \n", vector[1]);
+    printf("prom %d \n", vector[1]);
 
-  free(vector);
-  free(copia);
-  return (0);
+    free(vector);
+    free(copia);
+    return (0);
 }
 
 // array A[] has the items to sort; array B[] is a work array
-void BottomUpMergeSort(int A[], int B[],int n)
+int BottomUpMergeSort(int A[], int B[], int n)
 {
     // Each 1-element run in A is already "sorted".
     // Make successively longer sorted runs of length 2, 4, 8, 16... until whole array is sorted.
@@ -62,7 +62,7 @@ void BottomUpMergeSort(int A[], int B[],int n)
         {
             // Merge two runs: A[i:i+width-1] and A[i+width:i+2*width-1] to B[]
             // or copy A[i:n-1] to B[] ( if(i+width >= n) )
-            BottomUpMerge(A, i, min(i+width, n), min(i+2*width, n), B);
+            BottomUpMerge(A, i, min(i + width, n), min(i + 2 * width, n), B);
         }
         // Now work array B is full of runs of length 2*width.
         // Copy array B to array A for next iteration.
@@ -74,24 +74,28 @@ void BottomUpMergeSort(int A[], int B[],int n)
 
 //  Left run is A[iLeft :iRight-1].
 // Right run is A[iRight:iEnd-1  ].
-void BottomUpMerge(int A[], int iLeft, int iRight, int iEnd, int B[])
+int BottomUpMerge(int A[], int iLeft, int iRight, int iEnd, int B[])
 {
     int i = iLeft, j = iRight;
     // While there are elements in the left or right runs...
-    for (int k = iLeft; k < iEnd; k++) {
+    for (int k = iLeft; k < iEnd; k++)
+    {
         // If left run head exists and is <= existing right run head.
-        if (i < iRight && (j >= iEnd || A[i] <= A[j])) {
+        if (i < iRight && (j >= iEnd || A[i] <= A[j]))
+        {
             B[k] = A[i];
             i = i + 1;
-        } else {
-            B[k] = A[j];
-            j = j + 1;    
         }
-    } 
+        else
+        {
+            B[k] = A[j];
+            j = j + 1;
+        }
+    }
 }
 
-void CopyArray(int B[], int A[], int n)
+int CopyArray(int B[], int A[], int n)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         A[i] = B[i];
 }
